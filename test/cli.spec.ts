eb1fcf8d30d01379abb281f6c4d5c703c2e61bc9
mstssk <masato.sasaki+github@topgate.co.jp>
2016-07-05 13:53:10 +0900

    add '--with-query' and '--namespace' spec

--- test/cli.spec.ts
+++ test/cli.spec.ts
@@ -128,4 +128,19 @@ describe("CLI", () => {
                 done();
             });
     });
+
+    it("file to stdout with query params and namespace", (done) => {
+        const swaggerJsonPath = path.resolve(fixturesDir, "swagger.json");
+        const expectedPath = path.resolve(fixturesDir, "namespace.with-query-params.d.ts");
+        nexpect
+            .spawn("node", [sw2dtsPath, "--with-query", "-n", "foo", swaggerJsonPath])
+            .run((err, stdout, exit) => {
+                const actual = stdout.join("\n") + "\n";
+                const expected = fs.readFileSync(expectedPath).toString();
+                assert.equal(actual, expected);
+                assert(!err);
+                assert(exit === 0);
+                done();
+            });
+    });
 });
---
+++ test/fixtures/namespace.with-query-params.d.ts
@@ -0,0 +1,30 @@
+declare namespace foo {
+    export interface ApiDummyModelQueryParams {
+        required?: boolean;
+        cursor?: string;
+        limit?: number;
+        offset?: number;
+    }
+    export interface Dummy {
+        image?: Image;
+        createdAt?: string; // date-time
+        id?: string; // int64
+        kind?: string;
+        name?: string;
+        note?: string;
+        count?: number; // int32
+        updatedAt?: string; // date-time
+        ids?: string /* int64 */ [];
+    }
+    export interface Image {
+        createdAt?: string; // date-time
+        fileSize?: number; // int32
+        height?: number; // int32
+        id?: string; // int64
+        updatedAt?: string; // date-time
+        url?: string;
+        width?: number; // int32
+    }
+    export interface Noop {
+    }
+}
