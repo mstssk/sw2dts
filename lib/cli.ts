f1de100393528e3319315840d8f56e3c5052b912
mstssk <mstssk+github@gmail.com>
2019-03-23 03:23:13 +0900

    chore: format with prettier

--- lib/cli.ts
+++ lib/cli.ts
@@ -6,85 +6,84 @@ import { convert } from "./converter";
 const pkg = require("../package.json");
 
 interface RootOptions {
-    /** @deprecated */
-    stdin: boolean;
-    output: string[];
-    namespace: string[];
-    withQuery: boolean;
-    sortProps: boolean;
+  /** @deprecated */
+  stdin: boolean;
+  output: string[];
+  namespace: string[];
+  withQuery: boolean;
+  sortProps: boolean;
 }
 
 const root = commandpost
-    .create<RootOptions, { input_filename: string }>("sw2dts [input_filename]")
-    .version(pkg.version, "-v, --version")
-    .option("-w, --with-query", "With GET query parameters.")
-    .option("-s, --sort-props", "Sort type properties order.")
-    .option("-o, --output <output_filename>", "Output to file.")
-    .option("-n, --namespace <namespace>", "Use namespace.")
-    .option("--stdin", "[Deprecated] Input from standard input.")
-    .action(async (opts, args) => {
+  .create<RootOptions, { input_filename: string }>("sw2dts [input_filename]")
+  .version(pkg.version, "-v, --version")
+  .option("-w, --with-query", "With GET query parameters.")
+  .option("-s, --sort-props", "Sort type properties order.")
+  .option("-o, --output <output_filename>", "Output to file.")
+  .option("-n, --namespace <namespace>", "Use namespace.")
+  .option("--stdin", "[Deprecated] Input from standard input.")
+  .action(async (opts, args) => {
+    // TODO Delete '--stdin' option.
+    if (opts.stdin) {
+      console.warn("'--stdin' option is deprecated.");
+    }
+    if (args.input_filename && opts.stdin) {
+      process.stderr.write("Invalid parameters!\n");
+      process.exit(1);
+      return;
+    }
 
-        // TODO Delete '--stdin' option.
-        if (opts.stdin) {
-            console.warn("'--stdin' option is deprecated.");
-        }
-        if (args.input_filename && opts.stdin) {
-            process.stderr.write("Invalid parameters!\n");
-            process.exit(1);
-            return;
-        }
+    const outputFilename = opts.output[0];
+    const namespace = opts.namespace[0];
+    const withQuery = opts.withQuery;
+    const sortProps = opts.sortProps;
 
-        const outputFilename = opts.output[0];
-        const namespace = opts.namespace[0];
-        const withQuery = opts.withQuery;
-        const sortProps = opts.sortProps;
+    const input = await (args.input_filename ? fromFile(args.input_filename) : fromStdin());
+    const model = await convert(YAML.safeLoad(input), { namespace, withQuery, sortProps });
 
-        const input = await (args.input_filename ? fromFile(args.input_filename) : fromStdin());
-        const model = await convert(YAML.safeLoad(input), { namespace, withQuery, sortProps });
+    if (outputFilename) {
+      fs.writeFileSync(outputFilename, model);
+    } else {
+      process.stdout.write(model);
+    }
+    process.exit(0);
+  });
 
-        if (outputFilename) {
-            fs.writeFileSync(outputFilename, model);
-        } else {
-            process.stdout.write(model);
-        }
-        process.exit(0);
-    });
-
-commandpost
-    .exec(root, process.argv)
-    .catch(errorHandler);
+commandpost.exec(root, process.argv).catch(errorHandler);
 
 function fromStdin() {
-    process.stdin.setEncoding("utf-8");
-    return new Promise<string>((resolve, reject) => {
-        let data = "";
-        process.stdin
-            .on("data", chunk => data += chunk)
-            .once("end", () => resolve(data))
-            .once("error", err => reject(err));
-        setTimeout(() => {
-            if (data) { return; }
-            reject();
-        }, 1000);
-    });
+  process.stdin.setEncoding("utf-8");
+  return new Promise<string>((resolve, reject) => {
+    let data = "";
+    process.stdin
+      .on("data", chunk => (data += chunk))
+      .once("end", () => resolve(data))
+      .once("error", err => reject(err));
+    setTimeout(() => {
+      if (data) {
+        return;
+      }
+      reject();
+    }, 1000);
+  });
 }
 
 function fromFile(inputFileName: string) {
-    return new Promise<string>((resolve, reject) => {
-        resolve(fs.readFileSync(inputFileName, { encoding: "utf-8" }));
-    });
+  return new Promise<string>((resolve, reject) => {
+    resolve(fs.readFileSync(inputFileName, { encoding: "utf-8" }));
+  });
 }
 
 function errorHandler(err: Error) {
-    if (err == null) {
-        process.stdout.write(root.helpText());
-        process.exit(0);
-        return;
-    }
-    if (err instanceof Error) {
-        console.error(err.stack);
-    } else if (err) {
-        console.error(err);
-    }
-    process.exit(1);
+  if (err == null) {
+    process.stdout.write(root.helpText());
+    process.exit(0);
+    return;
+  }
+  if (err instanceof Error) {
+    console.error(err.stack);
+  } else if (err) {
+    console.error(err);
+  }
+  process.exit(1);
 }
--- lib/converter.ts
+++ lib/converter.ts
@@ -2,87 +2,90 @@ import dtsgen from "dtsgenerator";
 import { SwaggerSpec, PathDefinition, SchemaDefinition, SchemaProperties } from "../types/swagger";
 
 export async function convert(data: SwaggerSpec, options: ConverterOptions = {}) {
-    if (!options.nameResolver) {
-        options.nameResolver = PascalCaseNameResolver;
-    }
-    const jsonSchemas: SchemaDefinition[] = [];
-    for (const title in data.definitions) {
-        const schema = data.definitions[title];
-        fixRef(schema);
-        schema.id = title;
-        jsonSchemas.push(schema);
-    }
-    if (options.withQuery) {
-        for (const path in (data.paths || [])) {
-            const props = data.paths[path];
-            if (!props.get || isEmpty(props.get.parameters)
-                || props.get.parameters.some(v => v.in !== "query")) {
-                continue;
-            }
-            const required: string[] = [];
-            const properties = props.get.parameters.reduce<SchemaProperties>((result, value) => {
-                result[value.name] = {
-                    type: value.type,
-                    items: value.items,
-                    enum: value.enum,
-                };
-                if (value.required) {
-                    required.push(value.name);
-                }
-                return result;
-            }, {});
-            const schema: SchemaDefinition = {
-                id: options.nameResolver(path, props, options),
-                type: "object",
-                properties,
-                required,
-            };
-            jsonSchemas.push(schema);
+  if (!options.nameResolver) {
+    options.nameResolver = PascalCaseNameResolver;
+  }
+  const jsonSchemas: SchemaDefinition[] = [];
+  for (const title in data.definitions) {
+    const schema = data.definitions[title];
+    fixRef(schema);
+    schema.id = title;
+    jsonSchemas.push(schema);
+  }
+  if (options.withQuery) {
+    for (const path in data.paths || []) {
+      const props = data.paths[path];
+      if (!props.get || isEmpty(props.get.parameters) || props.get.parameters.some(v => v.in !== "query")) {
+        continue;
+      }
+      const required: string[] = [];
+      const properties = props.get.parameters.reduce<SchemaProperties>((result, value) => {
+        result[value.name] = {
+          type: value.type,
+          items: value.items,
+          enum: value.enum
+        };
+        if (value.required) {
+          required.push(value.name);
         }
+        return result;
+      }, {});
+      const schema: SchemaDefinition = {
+        id: options.nameResolver(path, props, options),
+        type: "object",
+        properties,
+        required
+      };
+      jsonSchemas.push(schema);
     }
-    if (options.sortProps) {
-        jsonSchemas.filter(sd => !!sd.properties).forEach(sd => {
-            sd.properties = Object.keys(sd.properties).sort().reduce<SchemaProperties>((result, key) => {
-                result[key] = sd.properties[key];
-                return result;
-            }, {});
-        });
-    }
-    return await dtsgen({ contents: jsonSchemas, namespaceName: options.namespace });
+  }
+  if (options.sortProps) {
+    jsonSchemas
+      .filter(sd => !!sd.properties)
+      .forEach(sd => {
+        sd.properties = Object.keys(sd.properties)
+          .sort()
+          .reduce<SchemaProperties>((result, key) => {
+            result[key] = sd.properties[key];
+            return result;
+          }, {});
+      });
+  }
+  return await dtsgen({ contents: jsonSchemas, namespaceName: options.namespace });
 }
 
 function fixRef(obj: Record<string, any>) {
-    for (const key in obj) {
-        if (key === "$ref") {
-            obj["$ref"] = obj["$ref"].split("/").pop();
-        } else if (typeof obj[key] === "object") {
-            fixRef(obj[key]);
-        }
+  for (const key in obj) {
+    if (key === "$ref") {
+      obj["$ref"] = obj["$ref"].split("/").pop();
+    } else if (typeof obj[key] === "object") {
+      fixRef(obj[key]);
     }
+  }
 }
 
 function isEmpty(array: ArrayLike<any>) {
-    return array ? !array.length : true;
+  return array ? !array.length : true;
 }
 
 /**
  * Default name resolver which resolve name as PascalCase from path string.
  */
 export function PascalCaseNameResolver(path: string, pathDefinition: PathDefinition, options: ConverterOptions) {
-    options = options || {};
-    path = path.replace(/{[^}]*}/g, '').replace(/\/$/, '');
-    path = path.replace(/(^|[\/_-])(\w)/g, (substr, ...args) => {
-        return args[1].toUpperCase();
-    });
-    path = path.replace(/(\d+)(\w)/, (substr, ...args) => {
-        return args[0] + args[1].toUpperCase();
-    });
-    return path;
+  options = options || {};
+  path = path.replace(/{[^}]*}/g, "").replace(/\/$/, "");
+  path = path.replace(/(^|[/_-])(\w)/g, (substr, ...args) => {
+    return args[1].toUpperCase();
+  });
+  path = path.replace(/(\d+)(\w)/, (substr, ...args) => {
+    return args[0] + args[1].toUpperCase();
+  });
+  return path;
 }
 
 export interface ConverterOptions {
-    namespace?: string;
-    withQuery?: boolean;
-    sortProps?: boolean;
-    nameResolver?: (path: string, pathDefinition: PathDefinition, options: ConverterOptions) => string;
+  namespace?: string;
+  withQuery?: boolean;
+  sortProps?: boolean;
+  nameResolver?: (path: string, pathDefinition: PathDefinition, options: ConverterOptions) => string;
 }
